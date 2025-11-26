d72750dd66aa61d957d8d2bd92e7c030a2de83d5
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-11-26 13:25:43 +0300

    run.py moved

--- server/run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render uses PORT environment variable
-    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False for production
